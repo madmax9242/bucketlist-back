1464e11538354b2d89fa81c9aa53eb3bb24b4f5b
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-06-10 00:13:02 -0400

    removing all but snapshot

--- src/main/java/com/gatherup/app/dao/EventDao.java
+++
@@ -1,8 +0,0 @@
-package com.gatherup.app.dao;
-
-import com.gatherup.app.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventDao extends JpaRepository<Event, Integer> {
-
-}
